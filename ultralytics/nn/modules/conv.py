6a833e95f8b3f3357cc81f6e4c8c0e5b21566256
tsuberok <tsuberok@ukr.net>
2026-02-13 14:50:44 +0200

    checking where i fucked up but changing bitconv into conv

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -93,30 +93,47 @@ class BitConv(nn.Conv2d):
 
 class BitConvDWsep(nn.Module):
     """Standard Bitnet convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    # default_act = nn.SiLU()  # default activation
+    # def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+    #     """Initialize Conv layer with given arguments including activation."""
+    #     super().__init__()
+    #     self.bitConvDepth = BitConv(toDequant=False, in_channels = c1, 
+    #                                 out_channels =c1, 
+    #                                 kernel_size =k, 
+    #                                 stride = s, 
+    #                                 padding = autopad(k, p, d), 
+    #                                 dilation = 1,
+    #                                 groups = c1,
+    #                                 bias = False)
+    #     self.bitConvSep = BitConv(toDequant=True, in_channels = c1, 
+    #                               out_channels = c2, 
+    #                               kernel_size=1, 
+    #                               stride = 1,
+    #                               padding= 0, 
+    #                               dilation=1,
+    #                               groups = 1,
+    #                               bias=False)
+    #     self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+    # def forward(self, x):
+    #     """Apply convolution, batch normalization and activation to input tensor."""
+    #     return self.act(self.bitConvSep(self.bitConvDepth(x)))
     default_act = nn.SiLU()  # default activation
+
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initialize Conv layer with given arguments including activation."""
         super().__init__()
-        self.bitConvDepth = BitConv(toDequant=False, in_channels = c1, 
-                                    out_channels =c1, 
-                                    kernel_size =k, 
-                                    stride = s, 
-                                    padding = autopad(k, p, d), 
-                                    dilation = 1,
-                                    groups = c1,
-                                    bias = False)
-        self.bitConvSep = BitConv(toDequant=True, in_channels = c1, 
-                                  out_channels = c2, 
-                                  kernel_size=1, 
-                                  stride = 1,
-                                  padding= 0, 
-                                  dilation=1,
-                                  groups = 1,
-                                  bias=False)
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
-        return self.act(self.bitConvSep(self.bitConvDepth(x)))
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Apply convolution and activation without batch normalization."""
+        return self.act(self.conv(x))
+
 
 
 
